1379546e5f1eb02ca6ab81781381945894ed5323
R136a1783 <144981148+R136a1783@users.noreply.github.com>
2023-09-21 04:37:09 +0000

    aaaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 # Streamlitライブラリをインポート
 import streamlit as st
 import random
-
 st.title("おみくじアプリ")
 if st.button("おみくじを引く"):
     results = ["大吉","中吉","小吉","吉","凶","大凶"]
-    results = random.choice(results)
+    result = random.choice(results)
     st.write(f"結果:{result}")
