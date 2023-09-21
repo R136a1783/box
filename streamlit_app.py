fade4be47876e5bee2c66940bafeb8aaefd32242
R136a1783 <144981148+R136a1783@users.noreply.github.com>
2023-09-21 04:52:25 +0000

    Viv

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title("おみくじアプリ")
 if st.button("おみくじを引く"):
     results = ["ロシア","中国","北朝鮮","ソマリア","アゼルバイジャン","アフガニスタン","ブリカス","ミャンマー"]
     result = random.choice(results)
-    st.write(f"私の出身地は:{result}です。")
+    st.write(f"私の出身地は{result}です。")
