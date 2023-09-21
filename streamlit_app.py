0451a034185c05ed3e5d29e4bfe7811ab1784392
R136a1783 <144981148+R136a1783@users.noreply.github.com>
2023-09-21 23:55:23 +0000

    hg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,33 @@
 # Streamlitライブラリをインポート
-import streamlit as st
 import random
-st.title("おみくじアプリ")
-if st.button("おみくじを引く"):
-    results = ["ロシア","中国","北朝鮮","ソマリア","アゼルバイジャン","アフガニスタン","ブリカス","ミャンマー"]
-    result = random.choice(results)
-    st.write(f"私の出身地は{result}です。")
+import requests
+
+# 軍歌のURLリストを取得する
+def get_military_song_urls():
+    response = requests.get("https://www.japanese-military-songs.com/")
+    html = response.text
+
+    song_urls = []
+    for a in html.split("<a href=\""):
+        if "/song/" in a:
+            song_urls.append("https://www.japanese-military-songs.com" + a)
+
+    return song_urls
+
+# ランダムで軍歌を再生する
+def play_random_military_song():
+    song_urls = get_military_song_urls()
+    song_url = random.choice(song_urls)
+
+    response = requests.get(song_url)
+    mp3_data = response.content
+
+    with open("military_song.mp3", "wb") as f:
+        f.write(mp3_data)
+
+    # 再生する
+    subprocess.call(["mpg123", "military_song.mp3"])
+
+# アプリケーションを起動する
+if __name__ == "__main__":
+    play_random_military_song()
