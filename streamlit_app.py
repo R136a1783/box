29fe91bc0632295d649acfeeadf9640effcb0c24
R136a1783 <144981148+R136a1783@users.noreply.github.com>
2023-09-21 04:32:01 +0000

    q

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import ramdom
 
 st.title("おみくじアプリ")
-if st.buttun("おみくじを引く"):
+if st.button("おみくじを引く"):
     results = ["大吉","中吉","小吉","吉","凶","大凶"]
     results = ramdom.choice(results)
     st.write(f"結果:{results}")
