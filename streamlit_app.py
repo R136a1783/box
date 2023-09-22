af43f90e57372260953f6aa95b073b98f466d0ae
R136a1783 <144981148+R136a1783@users.noreply.github.com>
2023-09-22 00:06:26 +0000

    h

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import requests
 def get_random_military_song():
   """日本の軍歌をランダムで取得する"""
   military_songs = ["軍艦マーチ", "皇国の守護者", "愛国行進曲", "海ゆかば", "出征壮行曲", "暁の水平線", "戦友", "新しき朝", "進め、進め", "南海の花"]
-  return military_songs[random.randint(0, len(military_songs) - 1)]
+  return military_songs = [random.randint(0, len(military_songs) - 1)]
 
 def play_military_song(military_song):
   """日本の軍歌を再生する"""
